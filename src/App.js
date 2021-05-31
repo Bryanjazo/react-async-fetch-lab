faea56bd00ff7812aa86eb7599342095e3649416
Bryan Jazo <bryanjazo88@gmail.com>
2021-05-31 12:11:57 -0700

    commit tree 0d153cd0146f77f5b747817aec87f9bf364a793d

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+    peoplaInSpace: []
+  }
+
+  render(){
+    return(
+      <div>
+      {this.state.peoplaInSpace.map(person => person.name)}
+    </div>
+    )
+  }
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        peoplaInSpace: data.people
+      })
+    })
+  }
+}
+
+export default App;
